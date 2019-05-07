dd647b3cf1ec69383533d2d991fd2ebf7d67743e
Andrew <aun.sokolov@gmail.com>
2019-05-07 19:00:41 +0300

    A little bit of cleanup

--- circular-buffer.lisp
+++ circular-buffer.lisp
@@ -4,156 +4,156 @@
 
 
 (defclass circular-buffer ()
- ((contents
-   :type (vector *)
-   :initform nil
-   :accessor home)
-  (front
-     :type integer
-     :accessor front
-     :initform 0)
-  (back
-     :type integer
-     :accessor back
-     :initform 0)
-  (actual-size
-     :type integer
-     :accessor actsize
-     :initform 0
-     :initarg :size)))
+  ((contents
+    :type (vector *)
+    :initform nil
+    :accessor home)
+   (front
+    :type integer
+    :accessor front
+    :initform 0)
+   (back
+    :type integer
+    :accessor back
+    :initform 0)
+   (actual-size
+    :type integer
+    :accessor actsize
+    :initform 0
+    :initarg :size)))
 
 (defmethod initialize-instance :after ((cirb circular-buffer)&key)
- (setf (home cirb) (make-array (max 2 (actsize cirb)) :initial-element nil) (actsize cirb) 0))
+  (setf (home cirb) (make-array (max 2 (actsize cirb)) :initial-element nil) (actsize cirb) 0))
 
 (defmethod cb-print ((cirb circular-buffer))
- (cbshow cirb)
- (info cirb))
+  (cbshow cirb)
+  (info cirb))
 
 (defmethod cbshow ((cirb circular-buffer))
- (format t "#CB( ")
- (iter (for elem in-vector (home cirb))
-  (format t "~s " elem))
- (format t ")~%"))
+  (format t "#CB( ")
+  (iter (for elem in-vector (home cirb))
+	(format t "~s " elem))
+  (format t ")~%"))
 
 (defmethod cb-coerce (seq)
- (let* ((len (length seq))
-        (res (make-instance 'circular-buffer :size (1+ len))))
-     (setf (front res) (1- len) (back res) 0 (actsize res) len)
-     (iter (for i from 0 to (1- len))
-      (setf (aref (home res) i) (elt seq i)))
-  res))
+  (let* ((len (length seq))
+         (res (make-instance 'circular-buffer :size (1+ len))))
+    (setf (front res) (1- len) (back res) 0 (actsize res) len)
+    (iter (for i from 0 to (1- len))
+	  (setf (aref (home res) i) (elt seq i)))
+    res))
 
 
 (defgeneric info (obj)
- (:documentation "Shows all the slots, except for contents themselves"))
+  (:documentation "Shows all the slots, except for contents themselves"))
 
 (defmethod info ((cirb circular-buffer))
- (format t "Front: ~s ~%Back: ~s ~%Actual-size ~s ~%" (front cirb) (back cirb) (actsize cirb)))
+  (format t "Front: ~s ~%Back: ~s ~%Actual-size ~s ~%" (front cirb) (back cirb) (actsize cirb)))
 
 (defmethod cbref ((cirb circular-buffer) index)
- (with-slots (contents front actual-size) cirb
-  (if (<= index (1- actual-size))
-   (aref contents (mod (+ front index) (length contents))))))
+  (with-slots (contents front actual-size) cirb
+    (if (<= index (1- actual-size))
+	(aref contents (mod (+ front index) (length contents))))))
 
 (defmethod update-circular-buffer-place ((cirb circular-buffer) index newelem)
- (with-slots (contents front actual-size) cirb
-  (if (<= index (1- actual-size))
-   (setf (aref contents (mod (+ front index) (length contents))) newelem))))
+  (with-slots (contents front actual-size) cirb
+    (if (<= index (1- actual-size))
+	(setf (aref contents (mod (+ front index) (length contents))) newelem))))
 
 (defsetf cbref update-circular-buffer-place)
 
 (defmethod cbemptyp ((cirb circular-buffer))
- (zerop (actsize cirb)))
+  (zerop (actsize cirb)))
 
 (defmethod cblength ((cirb circular-buffer))
- (actsize cirb))
+  (actsize cirb))
 
 (defgeneric ensure-capacity (cirb)
- (:documentation "Does the reallocation"))
+  (:documentation "Does the reallocation"))
 
 
 (defmethod push-front ((cirb circular-buffer) newelem)
- (with-slots (contents front actual-size) cirb
-  (when (= (length contents) actual-size) (extend-buffer cirb))
-  (setf front (mod (1- front) (length contents))
-        (aref contents front) newelem)
-  (incf actual-size)))
+  (with-slots (contents front actual-size) cirb
+    (when (= (length contents) actual-size) (extend-buffer cirb))
+    (setf front (mod (1- front) (length contents))
+          (aref contents front) newelem)
+    (incf actual-size)))
 
 (defmethod push-back ((cirb circular-buffer) newelem)
- (with-slots (contents back actual-size) cirb
-  (when (= (length contents) actual-size) (extend-buffer cirb))
-  (setf back (mod (1+ back) (length contents))
-        (aref contents back) newelem)
-  (incf actual-size)))
+  (with-slots (contents back actual-size) cirb
+    (when (= (length contents) actual-size) (extend-buffer cirb))
+    (setf back (mod (1+ back) (length contents))
+          (aref contents back) newelem)
+    (incf actual-size)))
 
 (defmethod pop-front ((cirb circular-buffer))
- (with-slots (contents actual-size front) cirb
-  (unless (zerop actual-size)
-   (when (> (length contents) (* 3 actual-size))
-    (shrink-buffer cirb))
-   (prog1 (aref contents front)
-          (setf front (mod (1+ front) (length contents)))
-          (decf actual-size)))))
+  (with-slots (contents actual-size front) cirb
+    (unless (zerop actual-size)
+      (when (> (length contents) (* 3 actual-size))
+	(shrink-buffer cirb))
+      (prog1 (aref contents front)
+        (setf front (mod (1+ front) (length contents)))
+        (decf actual-size)))))
 
 (defmethod pop-back ((cirb circular-buffer))
- (with-slots (contents actual-size back) cirb
-  (unless (zerop actual-size)
-   (when (> (length contents) (* 3 actual-size))
-    (shrink-buffer cirb))
-   (prog1 (aref contents back)
-          (setf back (mod (1- back) (length contents)))
-          (decf actual-size)))))
+  (with-slots (contents actual-size back) cirb
+    (unless (zerop actual-size)
+      (when (> (length contents) (* 3 actual-size))
+	(shrink-buffer cirb))
+      (prog1 (aref contents back)
+        (setf back (mod (1- back) (length contents)))
+        (decf actual-size)))))
 
 (defmethod peek-front ((cirb circular-buffer))
- (with-slots (contents front) cirb
-  (aref contents front)))
+  (with-slots (contents front) cirb
+    (aref contents front)))
 
 (defmethod peek-back ((cirb circular-buffer))
- (with-slots (contents back) cirb
-  (aref contents back)))
+  (with-slots (contents back) cirb
+    (aref contents back)))
 
 
 (defmacro circle-iterate (front back contents &rest body)
- `(let* ((len (length ,contents))
-         (first ,front)
-         (last ,back))
-    (if (<= first last)
-       (iter (for i from first to last)
-             (for element = (aref ,contents i))
-         ,@body)
-       (progn (iter (for i from first to (1- len))
-                    (for element = (aref ,contents i))
+  `(let* ((len (length ,contents))
+          (first ,front)
+          (last ,back))
+     (if (<= first last)
+	 (iter (for i from first to last)
+               (for element = (aref ,contents i))
                ,@body)
-              (iter (for i from 0 to last)
-                    (for element = (aref ,contents i))
-               ,@body)))))
+	 (progn (iter (for i from first to (1- len))
+                      (for element = (aref ,contents i))
+		      ,@body)
+		(iter (for i from 0 to last)
+                      (for element = (aref ,contents i))
+		      ,@body)))))
 
 
 
 (defmethod shape-buffer ((cirb circular-buffer) newsize default-element)
- (with-slots (contents front back) cirb
-  (let ((newhome (make-array newsize :initial-element nil))
-        (j 0))
-   (circle-iterate front back contents (setf (aref newhome j) element) (incf j))
-   (setf front 0 back (1- j))
-   (unless (= (1+ back) newsize)
-           (circle-iterate (mod (1+ back) newsize) (mod (1- front) newsize) newhome
-            (setf (aref newhome i) (and default-element (funcall default-element)))))
-   (setf contents newhome))))
+  (with-slots (contents front back) cirb
+    (let ((newhome (make-array newsize :initial-element nil))
+          (j 0))
+      (circle-iterate front back contents (setf (aref newhome j) element) (incf j))
+      (setf front 0 back (1- j))
+      (unless (= (1+ back) newsize)
+        (circle-iterate (mod (1+ back) newsize) (mod (1- front) newsize) newhome
+			(setf (aref newhome i) (and default-element (funcall default-element)))))
+      (setf contents newhome))))
 
 
 (defgeneric extend-buffer (cirb &optional fact with-default)
- (:documentation "Reallocates buffer, making it bigger. Triggers when actual size is equal
+  (:documentation "Reallocates buffer, making it bigger. Triggers when actual size is equal
 to vector size, extends by factor given"))
 
 (defmethod extend-buffer ((cirb circular-buffer) &optional (fact 2) (with-default nil))
- (with-slots (actual-size) cirb
-  (shape-buffer cirb (ceiling (* fact actual-size)) with-default)))
+  (with-slots (actual-size) cirb
+    (shape-buffer cirb (ceiling (* fact actual-size)) with-default)))
 
 (defgeneric shrink-buffer (cirb &optional with-default)
- (:documentation "Reallocates buffer, making it smaller. Triggers when actual size is less
+  (:documentation "Reallocates buffer, making it smaller. Triggers when actual size is less
 then 1/3, shrink to the exact size"))
 
 (defmethod shrink-buffer ((cirb circular-buffer) &optional (with-default nil))
- (with-slots (actual-size) cirb
-  (shape-buffer cirb (* 2 actual-size) with-default)))
+  (with-slots (actual-size) cirb
+    (shape-buffer cirb (* 2 actual-size) with-default)))
--- deque.lisp
+++ deque.lisp
@@ -30,20 +30,58 @@
     :accessor bl-index
     :initarg :ind)))
 
+(defgeneric dqlength (deq)
+  (:documentation "Returns length; static"))
+
+(defgeneric dqemptyp (deq)
+  (:documentation "T if empty, nil otherwise; static"))
+
+(defgeneric dq-coerce (vector-sequence)
+  (:documentation "Coerces vector into deque and returns that deque"))
+
+(defgeneric peek-front (deq)
+  (:documentation "Returns first element; static"))
+
+(defgeneric peek-back (deq)
+  (:documentation "Returns last element; static"))
+
+(defgeneric dqref (deq index)
+  (:documentation "Returns element by index, counting from the start; static"))
+
+(defgeneric print-readable-deque (deq &optional start end)
+  (:documentation "Prints deque fromt start to end in readable form, enclosed in []"))
+
+(defgeneric push-front (deq newelem)
+  (:documentation "Pushes newelem to the front of deque; returns new length"))
+
+(defgeneric push-back (deq newelem)
+  (:documentation "Pushes newelem to the back of deque; returns new length"))
+
+(defgeneric pop-front (deq)
+  (:documentation "Pops first element of the deque and returns it;
+                  does not delete it; works only if deque is not empty"))
+
+(defgeneric pop-back (deq)
+  (:documentation "Pops last element of the deque and returns it;
+                  does not delete it; works only if deque is not empty"))
+
+;;;; ========================================= Implementation =========================================
 
 (defmethod initialize-instance :after ((deq deque) &key)
   (iter (for i from 0 to (1- (length (home deq))))
-    (setf (aref (home deq) i) (make-instance 'dblock :ind i))))
+	(setf (aref (home deq) i) (make-instance 'dblock :ind i))))
 
 (defmethod dqshow ((deq deque))
+  "Prints contetns slot; not for users"
   (format t "#DQ( ")
   (iter (for dblck in-vector (home deq))
-    (iter (for elem in-vector (bl-contents dblck))
-      (format t "~s " elem))
-    (format t "|| "))
+	(iter (for elem in-vector (bl-contents dblck))
+	      (format t "~s " elem))
+	(format t "|| "))
   (format t ")~%"))
 
 (defmethod info :after ((deq deque))
+  "Prints other slots; not for users"
   (format t "Contents size ~s ~%" (contsize deq)))
 
 (defmethod dq-print ((deq deque))
@@ -57,138 +95,116 @@
 
 (defmethod peek-back ((deq deque))
   (with-slots (contents back) deq
-   (with-slots (block-contents block-back) (aref contents back)
-    (aref block-contents block-back))))
+    (with-slots (block-contents block-back) (aref contents back)
+      (aref block-contents block-back))))
 
 (defmethod dqref ((deq deque) ind)
   (with-slots (contents front) deq
-    (with-slots (block-contents block-front) (aref contents front)
+    (with-slots (block-front) (aref contents front)
       (multiple-value-bind (column place) (floor (+ block-front ind) 128)
-       (aref (cbref deq column) place)))))
+	(with-slots (block-contents) (cbref deq column)
+          (aref block-contents place))))))
 
 (defmethod update-deque-place ((deq deque) ind newelem)
+  "Updates element on given place by newvalue; for setf, not for users"
   (if (<= ind (1- (contsize deq)))
       (with-slots (contents front) deq
-       (with-slots (block-contents block-front) (aref contents front)
+	(with-slots (block-contents block-front) (aref contents front)
           (multiple-value-bind (column place) (floor (+ block-front ind) 128)
-                   (setf (aref (cbref deq column) place) newelem))))))
+            (setf (aref (cbref deq column) place) newelem))))))
 
 (defsetf dqref update-deque-place)
 
 (defmethod dqemptyp ((deq deque))
- (zerop (contsize deq)))
+  (zerop (contsize deq)))
 
 (defmethod dqlength ((deq deque))
- (contsize deq))
+  (contsize deq))
 
 (defmethod dq-coerce (seq)
   (let* ((block-amount (1+ (floor (length seq) 128)))
          (res (make-instance 'deque :size block-amount)))
     (with-slots (contents front back actual-size contents-size) res
       (iter (for i from 0 to (1- (length seq)))
-        (multiple-value-bind (m n) (floor i 128)
-          (setf (aref (bl-contents (aref contents m)) n) (aref seq i)
-                (bl-front (aref contents m)) 0
-                (bl-back (aref contents m)) (if (< m (floor (1- (length seq)) 128)) 127 n))))
+            (multiple-value-bind (m n) (floor i 128)
+              (setf (aref (bl-contents (aref contents m)) n) (aref seq i)
+                    (bl-front (aref contents m)) 0
+                    (bl-back (aref contents m)) (if (< m (floor (1- (length seq)) 128)) 127 n))))
       (setf front 0 back (floor (1- (length seq)) 128)
             actual-size (1+ (floor (1- (length seq)) 128))
             contents-size (length seq)))
     res))
 
+(defmethod print-readable-deque ((deq deque) &optional (start 0) (end nil))
+  (with-slots (contents front contents-size) deq
+    (let ((step start) (last (or end contents-size)))
+      (format t "[")
+      (iter (for i from step to (1- last))
+            (format t "~s" (dqref deq i))
+            (unless (= (1- last) i) (format t " ")))
+      (format t "]"))))
+
+
 (defmethod shape-buffer ((deq deque) newsize default-element)
- (call-next-method)
- (with-slots (contents front back) deq
-   (unless (= (mod (1+ back) (length contents)) front)
-           (circle-iterate (mod (1+ back) (length contents)) (mod (1- front) (length contents)) contents
-                           (setf (bl-index (aref contents i)) i)))))
+  (call-next-method)
+  (with-slots (contents front back) deq
+    (unless (= (mod (1+ back) (length contents)) front)
+      (circle-iterate (mod (1+ back) (length contents)) (mod (1- front) (length contents)) contents
+                      (setf (bl-index (aref contents i)) i)))))
 
 (defmethod push-front ((deq deque) newelem)
- (with-slots (contents front actual-size contents-size) deq
-   (with-slots (block-contents block-front block-index) (aref contents front)
-    (if (zerop block-front)
-        (progn (when (= actual-size (length contents)) (extend-buffer deq 2 (lambda () (make-instance 'dblock))))
-               (setf front (mod (1- front) (length contents)))
-               (incf actual-size)
-               (with-slots (block-contents block-front) (aref contents front)
-                           (setf (aref block-contents 127) newelem block-front 127)))
-        (progn (decf block-front)
-               (setf (aref block-contents block-front) newelem)))
-    (incf contents-size))))
+  (with-slots (contents front actual-size contents-size) deq
+    (with-slots (block-contents block-front block-index) (aref contents front)
+      (if (zerop block-front)
+          (progn (when (= actual-size (length contents)) (extend-buffer deq 2 (lambda () (make-instance 'dblock))))
+		 (setf front (mod (1- front) (length contents)))
+		 (incf actual-size)
+		 (with-slots (block-contents block-front) (aref contents front)
+                   (setf (aref block-contents 127) newelem block-front 127)))
+          (progn (decf block-front)
+		 (setf (aref block-contents block-front) newelem)))
+      (incf contents-size))))
 
 (defmethod push-back ((deq deque) newelem)
- (with-slots (contents back actual-size contents-size) deq
-   (with-slots (block-contents block-back block-index) (aref contents back)
-    (if (= 127 block-back)
-        (progn (when (= actual-size (length contents)) (extend-buffer deq 2 (lambda () (make-instance 'dblock))))
-               (setf back (mod (1+ back) (length contents)))
-               (incf actual-size)
-               (with-slots (block-contents block-back) (aref contents back)
-                           (setf (aref block-contents 0) newelem block-back 0)))
-        (progn (incf block-back)
-               (setf (aref block-contents block-back) newelem)))
-    (incf contents-size))))
+  (with-slots (contents back actual-size contents-size) deq
+    (with-slots (block-contents block-back block-index) (aref contents back)
+      (if (= 127 block-back)
+          (progn (when (= actual-size (length contents)) (extend-buffer deq 2 (lambda () (make-instance 'dblock))))
+		 (setf back (mod (1+ back) (length contents)))
+		 (incf actual-size)
+		 (with-slots (block-contents block-back) (aref contents back)
+                   (setf (aref block-contents 0) newelem block-back 0)))
+          (progn (incf block-back)
+		 (setf (aref block-contents block-back) newelem)))
+      (incf contents-size))))
 
 
 (defmethod pop-back ((deq deque))
- (with-slots (contents back actual-size contents-size) deq
-  (unless (zerop contents-size)
-   (with-slots (block-contents block-back) (aref contents back)
-    (let ((return-value (aref block-contents block-back)))
-     (decf contents-size)
-     (if (zerop block-back)
-         (progn (incf block-back 127)
-                (setf back (mod (1- back) (length contents)))
-                (decf actual-size)
-                (when (> (length contents) (* 3 actual-size))
-                  (shrink-buffer deq (lambda () (make-instance 'dblock)))))
-         (decf block-back))
-     return-value)))))
+  (with-slots (contents back actual-size contents-size) deq
+    (unless (zerop contents-size)
+      (with-slots (block-contents block-back) (aref contents back)
+	(let ((return-value (aref block-contents block-back)))
+	  (decf contents-size)
+	  (if (zerop block-back)
+              (progn (incf block-back 127)
+                     (setf back (mod (1- back) (length contents)))
+                     (decf actual-size)
+                     (when (> (length contents) (* 3 actual-size))
+                       (shrink-buffer deq (lambda () (make-instance 'dblock)))))
+              (decf block-back))
+	  return-value)))))
 
 (defmethod pop-front ((deq deque))
- (with-slots (contents front actual-size contents-size) deq
-  (unless (zerop contents-size)
-   (with-slots (block-contents block-front) (aref contents front)
-    (let ((return-value (aref block-contents block-front)))
-     (decf contents-size)
-     (if (= 127 block-front)
-         (progn (decf block-front 127)
-                (setf front (mod (1+ front) (length contents)))
-                (decf actual-size)
-                (when (> (length contents) (* 3 actual-size))
-                  (shrink-buffer deq (lambda () (make-instance 'dblock)))))
-         (incf block-front))
-     return-value)))))
-
-
-
-;;;;------------------------------------------------------------------------------------
-;;;;------------------------------------------------------------------------------------
-;;;;------------------------------------------------------------------------------------
-;;;;------------------------------------------------------------------------------------
-(defun make-num-vec (size)
- (let ((res (make-array size :element-type 'fixnum)))
-  (dotimes (i size) (setf (aref res i) i))
-  res))
-
-
-
-(defun vector-test (upto)
-  (let ((vec (make-array 0 :adjustable t :element-type 'fixnum :fill-pointer 0)))
-    (declare (type (vector fixnum *) vec))
-    (loop :for i from 0 to upto
-     do (progn (loop :for j from i downto (floor i 2)
-                do (unless (emptyp vec) (vector-pop vec)))
-         (loop :for j from (floor i 2) to i
-          do (vector-push-extend i vec))
-         (vector-push-extend i vec))))
-  t)
-
-(defun cb-test (upto)
-  (let ((vec (make-instance 'circular-buffer :size 1)))
-    (loop :for i from 0 to upto
-     do (progn (loop :for j from i downto (floor i 2)
-                do (unless (cbemptyp vec) (pop-back vec)))
-         (loop :for j from (floor i 2) to i
-          do (push-front vec i))
-         (push-back vec i))))
-  t)
+  (with-slots (contents front actual-size contents-size) deq
+    (unless (zerop contents-size)
+      (with-slots (block-contents block-front) (aref contents front)
+	(let ((return-value (aref block-contents block-front)))
+	  (decf contents-size)
+	  (if (= 127 block-front)
+              (progn (decf block-front 127)
+                     (setf front (mod (1+ front) (length contents)))
+                     (decf actual-size)
+                     (when (> (length contents) (* 3 actual-size))
+                       (shrink-buffer deq (lambda () (make-instance 'dblock)))))
+              (incf block-front))
+	  return-value)))))
